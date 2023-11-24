931feb033e31b573b39bca9758d94d71d47e29d5
Lorenzo <lorenzodevelopment301@gmail.com>
2023-11-24 08:46:34 -0600

    se empieza projects

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../Styles/Footer.css';
 import Logo from '../Images/LogoHD.png';
-import { FaInstagram, FaFacebook, FaLinkedin, FaWhatsapp } from 'react-icons/fa';
+import { FaInstagram, FaFacebook, FaLinkedin, FaWhatsapp, FaGithub } from 'react-icons/fa';
 
 export const Footer = () => {
   const year = new Date().getFullYear();
@@ -10,7 +10,7 @@ export const Footer = () => {
     <footer className='d-flex justify-content-center align-items-center pt-4'>
       <div className='w-100 d-flex flex-column justify-content-center align-items-center'>
         <div>
-          <img className='LogoWhite' src={Logo} alt='' />
+          <img loading='lazy' className='LogoWhite' src={Logo} alt='' />
         </div>
         <div className='d-flex flex-row align-items-center justify-content-center w-50 containerLinkFooter p-2 p-lg-5 pt-2 pt-lg-3'>
           <hr className='hrFooter m-0 p-0 me-2' />
@@ -20,11 +20,14 @@ export const Footer = () => {
           <a className='footerLink' target='_blank' rel='noopener noreferrer' href='https://www.facebook.com/profile.php?id=100091605591871'>
             <FaFacebook />
           </a>
-          <a className='footerLink' target='_blank' rel='noopener noreferrer' href='#'>
+          <a className='footerLink' target='_blank' rel='noopener noreferrer' href='https://www.linkedin.com/in/lorenzojimenezabarca301/'>
             <FaLinkedin />
           </a>
           <a className='footerLink' target='_blank' rel='noopener noreferrer' href='https://api.whatsapp.com/send?phone=87861758'>
             <FaWhatsapp />
+          </a>
+          <a className='footerLink' target='_blank' rel='noopener noreferrer' href='https://github.com/lorenzoJimenez301'>
+            <FaGithub />
           </a>
           <hr className='hrFooter m-0 p-0 ms-2' />
         </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,11 +8,9 @@ import { FaMoon } from 'react-icons/fa';
 import { MdSunny } from 'react-icons/md';
 import { useMediaQuery } from 'react-responsive';
 import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
-import { CSSTransition } from 'react-transition-group';
 import samuraiBlanco from '../Images/Samurai_White.png';
 import samuraiNegro from '../Images/Samurai_Black.png';
 import { useImage } from 'react-image';
-import { useEffect } from 'react';
 
 
 export const Navbar = ({ onImagenCambiada }) => {
@@ -26,10 +24,6 @@ export const Navbar = ({ onImagenCambiada }) => {
     });
     const [foto, setFoto] = useState(Logo);
     const [isOn, setIsOn] = useState(false);
-    const [activo, setActivo] = useState(true);
-    const active = () => {
-        setActivo(!activo);
-    }
     const isMobile = useMediaQuery({ maxWidth: 992 });
     const root = document.documentElement;
     const grisPrincipal = '#1D1D1D';
@@ -40,6 +34,7 @@ export const Navbar = ({ onImagenCambiada }) => {
         if (!isOn) {
             setFoto(LogoBlanco);
             root.style.setProperty('--piel', grisPrincipal);
+            root.style.setProperty('--pielEspecial', grisPrincipal);
             root.style.setProperty('--negro', '#ffffff');
             root.style.setProperty('--grisPrincipal', pielPrincipal);
             onImagenCambiada(srcImagen1);
@@ -52,26 +47,28 @@ export const Navbar = ({ onImagenCambiada }) => {
         }
 
     }
-
     const spring = {
         type: 'spring',
         stiffness: 700,
         damping: 30
     };
+    const [activo, setActivo] = useState(true);
+    const active = () => {
+        setActivo(!activo);
+    }
 
-
-    return (      
+    return (
         <nav className={`navBar position-${isMobile ? 'absolute' : 'relative'} navbar navbar-expand-lg m-0 p-0`}>
             <div style={{ backgroundColor: 'transparent' }} className='navBar2 container-fluid d-flex flex-row-reverse flex-lg-row justify-content-between align-items-center px-0'>
                 <a href='/'><img src={foto} className='logoPrincipal me-0 ' alt='logo' /></a>
-                {activo ? <RxHamburgerMenu onClick={active} id='hambur' className='d-lg-none' /> : <RxCross1 onClick={active} id='hambur' className='d-lg-none'  />}
+                {activo ? <RxHamburgerMenu onClick={active} id='hambur' className='d-lg-none' /> : <RxCross1 onClick={active} id='hambur' className='d-lg-none' />}
                 <div className='navbar-collapse d-lg-flex justify-content-center justify-content-lg-end align-items-center'>
                     <ul className={`${!activo ? 'collapsar' : ''} navbar-nav navList d-flex align-items-center justify-content-evenly`}>
-                        <li className='navItemContainer'><Link className='text-decoration-none navItem' data-ison={isOn} to={'/'}>Menu</Link></li>
-                        <li className='navItemContainer'><Link className='text-decoration-none navItem' data-ison={isOn} to={'/AboutMe'}>About Me</Link></li>
-                        <li className='navItemContainer'><Link className='text-decoration-none navItem' data-ison={isOn} to={'/Certifications'}>Certifications</Link></li>
-                        <li className='navItemContainer'><Link className='text-decoration-none navItem' data-ison={isOn} to={'/Projects'}>Projects</Link></li>
-                        <li className='navItemContainer'><Link className='text-decoration-none navItem' data-ison={isOn} to={'/Contact'}>Contact</Link></li>
+                        <li className='navItemContainer'><Link className='text-decoration-none navItem' data-ison={isOn} onClick={isMobile ? active : null} to={'/'} >Menu</Link></li>
+                        <li className='navItemContainer'><Link className='text-decoration-none navItem' data-ison={isOn} onClick={isMobile ? active : null} to={'/AboutMe'} >About Me</Link></li>
+                        <li className='navItemContainer'><Link className='text-decoration-none navItem' data-ison={isOn} onClick={isMobile ? active : null} to={'/Certifications'} >Certifications</Link></li>
+                        <li className='navItemContainer'><Link className='text-decoration-none navItem' data-ison={isOn} onClick={isMobile ? active : null} to={'/Projects'} >Projects</Link></li>
+                        <li className='navItemContainer'><Link className='text-decoration-none navItem' data-ison={isOn} onClick={isMobile ? active : null} to={'/Contact'} >Contact</Link></li>
                         <li className='navItemContainer d-none d-lg-flex align-items-center' >
                             <div className='switch' data-ison={isOn} onClick={toggleSwitch}>
                                 <motion.div className='handle' layout transition={spring} />
