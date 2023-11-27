41f3fdcc1bc2bb33e3739d9b884a2db554b84ece
Lorenzo <lorenzodevelopment301@gmail.com>
2023-11-26 19:23:06 -0600

    se termino el nav

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -64,7 +64,7 @@ export const Navbar = ({ onImagenCambiada }) => {
                 {activo ? <RxHamburgerMenu onClick={active} id='hambur' className='d-lg-none' /> : <RxCross1 onClick={active} id='hambur' className='d-lg-none' />}
             </div>
             <div className={`${!activo ? 'collapsar' : ''} navBar2`}>
-                <ul className={`${!activo ? 'collapsar' : ''} navbar-nav navList d-flex align-items-center justify-content-evenly`}>
+                <ul className={`${!activo ? 'collapsar' : ''} navList`}>
                     <li className='navItemContainer'><Link className='text-decoration-none navItem' data-ison={isOn} onClick={isMobile ? active : null} to={'/'} >Menu</Link></li>
                     <li className='navItemContainer'><Link className='text-decoration-none navItem' data-ison={isOn} onClick={isMobile ? active : null} to={'/AboutMe'} >About Me</Link></li>
                     <li className='navItemContainer'><Link className='text-decoration-none navItem' data-ison={isOn} onClick={isMobile ? active : null} to={'/Certifications'} >Certifications</Link></li>
