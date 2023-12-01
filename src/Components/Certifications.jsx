5872b43a2bef8d08d3f5208feed38546690441a4
Lorenzo <lorenzodevelopment301@gmail.com>
2023-12-01 11:31:41 -0600

    Se perfecciona el apartado proyectos

--- src/Components/Certifications.jsx
+++ src/Components/Certifications.jsx
@@ -11,8 +11,6 @@ import 'swiper/css/effect-fade';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
-import { RxCross1 } from "react-icons/rx";
-import { useMediaQuery } from 'react-responsive';
 import { ThemeContext } from './ThemeContext';
 import { useContext } from 'react';
 
@@ -36,59 +34,22 @@ const MySlider = () => {
             <SwiperSlide style={{ backgroundColor: 'var(--piel)' }} className='swiper-slide text-center d-flex flex-column align-items-center'>
                 <img alt='img' loading='lazy' className='imgCarrusel' src={cert1} />
                 <h3 className='titleCerti'>Responsive Web Design</h3>
-                <button className='btnMore' data-bs-toggle='modal' data-bs-target='#ResponsiveWeb'>More</button>
             </SwiperSlide>
             <SwiperSlide style={{ backgroundColor: 'var(--piel)' }} className='swiper-slide text-center d-flex flex-column align-items-center'>
                 <img alt='img' loading='lazy' className='imgCarrusel' src={cert2} />
                 <h3 className='titleCerti'>Front End Developer</h3>
-                <button className='btnMore' data-bs-toggle='modal' data-bs-target='#frontEndLibraries'>More</button>
             </SwiperSlide>
             <SwiperSlide style={{ backgroundColor: 'var(--piel)' }} className='swiper-slide text-center d-flex flex-column align-items-center'>
                 <img alt='img' loading='lazy' className='imgCarrusel' src={cert3} />
                 <h3 className='titleCerti'>Soft Skills</h3>
-                <button className='btnMore' data-bs-toggle='modal' data-bs-target='#softSkills'>More</button>
             </SwiperSlide>
             <SwiperSlide style={{ backgroundColor: 'var(--piel)' }} className='swiper-slide text-center d-flex flex-column align-items-center'>
                 <img alt='img' loading='lazy' className='imgCarrusel' src={cert4} />
                 <h3 className='titleCerti'>High school</h3>
-                <button className='btnMore' data-bs-toggle='modal' data-bs-target='#bachi'>More</button>
             </SwiperSlide>
         </Swiper>
     );
 };
-
-const Modal = ({ id, title, img, provider, desc }) => {
-
-    const isMobile = useMediaQuery({ maxWidth: 992 });
-
-    return (
-        <div className='modal fade' id={`${id}`} data-bs-backdrop='static' data-bs-keyboard='false' aria-labelledby='staticBackdropLabel' aria-hidden='true'>
-            <div className='modal-dialog modal-dialog-centered p-0 m-0'>
-                <div style={{ fontFamily: 'var(--mont)' }} className='modal-content modelCerti m-auto'>
-                    <div className='modal-header'>
-                        <h2 className='p-0 m-0' style={{ fontFamily: 'var(--mont)', fontWeight: 'bold', fontSize: `${isMobile ? '5vw' : '2vw'}` }} >{title}</h2>
-                        <RxCross1 style={{ color: '#000', fontSize: `${isMobile ? '6.5vw' : '1.7vw'}` }} type="button" className="btnclose" data-bs-dismiss="modal" aria-label="Close" />
-                    </div>
-                    <div className='d-flex flex-column flex-lg-row align-items-center modal-body'>
-                        <div className='w-50 d-flex justify-content-center align-items-center'>
-                            <img loading='lazy' className='imgModal' src={img} alt="imgModal" />
-                        </div>
-                        <div style={{ width: `${isMobile ? '100' : '50'}` }} className='textModalContainer text-center d-flex flex-column justify-content-center align-items-center'>
-                            <div style={{ gap: '.8vw', padding: `${isMobile ? '3vw' : ''}` }} className='d-flex flex-row align-items-center justify-content-center'>
-                                <h3 style={{ fontFamily: 'var(--mont)', fontWeight: 'bold', fontSize: `${isMobile ? '5vw' : '1.2vw'}` }} className='p-0 m-0'>Provider:</h3>
-                                <p style={{ fontFamily: 'var(--mont)', fontSize: `${isMobile ? '4vw' : '1vw'}` }} className='p-0 pt-0 m-0 textCerti'>{provider}</p>
-                            </div>
-                            <div style={{ marginTop: '2vw', padding: '5vw' }} className='d-flex flex-column align-items-center'>
-                                <h3 style={{ fontFamily: 'var(--mont)', fontWeight: 'bold', fontSize: `${isMobile ? '5vw' : '1.2vw'}` }} className='p-0 m-0'>Description:</h3>
-                                <p style={{ fontFamily: 'var(--mont)', fontSize: `${isMobile ? '3.5vw' : '1vw'}`, marginTop: '1vw' }} className='p-0'>{desc}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-}
 
 export const Certifications = () => {
     const { backgroundImage } = useContext(ThemeContext);
@@ -104,10 +65,6 @@ export const Certifications = () => {
                 <BsChevronLeft className="custom-prev-button btnNavigation " />
                 <MySlider />
                 <BsChevronRight className="custom-next-button btnNavigation " />
-                <Modal provider={'freeCodeCamp'} desc={'The freeCodeCamp Responsive Web Design certification is an educational program focused on teaching how to design and develop websites that adapt and are functional on different devices and screen sizes.'} img={cert1} title={'Responsive Web Design'} id={'ResponsiveWeb'} />
-                <Modal provider={'freeCodeCamp'} desc={'This certification validates skill in front-end development, focusing on the use of essential libraries and frameworks in building dynamic and attractive user interfaces. The curriculum includes mastery of popular libraries such as React.js and Redux, as well as advanced DOM manipulation with jQuery. Holders of this certification are equipped with the skills necessary to create interactive and optimized user experiences for various devices.'} img={cert2} title={'Front End Developer'} id={'frontEndLibraries'} />
-                <Modal provider={'COVAO'} desc={'This section highlights interpersonal and emotional skills that complement and strengthen my technical competencies. It includes skills such as effective communication, teamwork, problem solving, adaptability and leadership, essential for successful collaboration and balanced performance in professional environments.'} img={cert3} title={'Soft Skills'} id={'softSkills'} />
-                <Modal provider={'LIMERE'} desc={'This title certifies the successful completion of the secondary educational level. It provides a solid academic foundation, covering areas such as mathematics, science, humanities and arts. Graduates acquire critical thinking skills, effective communication, and preparation for higher education or entry into the workforce.'} img={cert4} title={'High school'} id={'bachi'} />
             </article>
         </section>
     );
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,30 +1,27 @@
 import React from 'react';
 import { useState } from 'react';
+import { ThemeContext } from './ThemeContext';
+import { useContext } from 'react';
 import '../Styles/Projects.css';
-import multityr from '../Images/Projects/Multityr.png';
-import Pagina_multityr from '../Projects_Download/MultiserviciosTyR-mulrityr.zip';
-import imgCalcu from '../Images/Projects/Calcu.png';
-import calcu from '../Projects_Download/calculadora-react.rar';
-import imgContador from '../Images/Projects/Contador.png';
-import contador from '../Projects_Download/contador-clics.rar';
-import imgToDo from '../Images/Projects/TODO.png';
-import toDo from '../Projects_Download/gestor-de-tareas.rar';
-import imgPhp from '../Images/Projects/php.png';
-import phpCrud from '../Projects_Download/PHP_CRUD.rar';
-import { IoMdDownload } from "react-icons/io";
-import { AiOutlineInfoCircle } from "react-icons/ai";
 import { RxCross1 } from "react-icons/rx";
+import { BsGithub, BsGlobe2, BsInfoCircle } from "react-icons/bs";
 import { useMediaQuery } from 'react-responsive';
+import apiLogo from '../Images/LogoTecno/apiLogo.png'
+import fakeStore from '../Images/Projects/fakeStore.png';
 import Node from '../Images/LogoTecno/NODE LOGO.png';
 import Html from '../Images/LogoTecno/HTML LOGO.png';
 import Css from '../Images/LogoTecno/CSS LOGO.png';
 import Php from '../Images/LogoTecno/PHP LOGO.png';
 import react from '../Images/LogoTecno/REACT LOGO.png';
 import Bootstrap from '../Images/LogoTecno/BOOTSTRAP LOGO.png';
-import { ThemeContext } from './ThemeContext';
-import { useContext } from 'react';
+import multityr from '../Images/Projects/Multityr.png';
+import imgCalcu from '../Images/Projects/Calcu.png';
+import imgContador from '../Images/Projects/Contador.png';
+import imgToDo from '../Images/Projects/TODO.png';
+import imgPhp from '../Images/Projects/php.png';
 
-const ProjectItem = ({ img, url, id }) => {
+
+const ProjectItem = ({ img, id, urlGit, urlPage }) => {
 
     const [hovered, setHovered] = useState(false);
 
@@ -38,9 +35,10 @@ const ProjectItem = ({ img, url, id }) => {
     return (
         <div className='projectItemContainer'>
             <div onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className='projectInfoContainer d-flex flex-column-reverse'>
-                <div className={`projectInfo ${hovered ? 'isHover' : ''}`}>
-                    <a className='aProject' download href={url}><IoMdDownload className='projectOpc down position-absolute' /></a>
-                    <AiOutlineInfoCircle data-bs-toggle='modal' data-bs-target={`#${id}`} className='projectOpc' />
+                <div className={`p-2 projectInfo ${hovered ? 'isHover' : ''}`}>
+                    <a className='linkProject d-flex justify-content-center align-items-center' target='_blank' href={urlGit}><BsGithub  className='projectOpc' /></a>
+                    <a className='linkProject d-flex justify-content-center align-items-center' target='_blank' href={urlPage}><BsGlobe2  className='projectOpc' /></a>
+                    <BsInfoCircle data-bs-toggle='modal' data-bs-target={`#${id}`} className='linkProject projectOpc' />
                 </div>
                 <img loading='lazy' className='projectImg' src={img} alt="project" />
             </div>
@@ -48,31 +46,25 @@ const ProjectItem = ({ img, url, id }) => {
     )
 }
 
-const ModalProject = ({ id, title, desc, img, tecno1, tecno2, tecno3, tecno4 }) => {
+const ModalProject = ({ id, title, img, tecno1, tecno2, tecno3, tecno4 }) => {
 
     const isMobile = useMediaQuery({ maxWidth: 992 })
     return (
-        <div id={id} className='modal fade' data-bs-backdrop='static' data-bs-keyboard='false' aria-labelledby='staticBackdropLabel' aria-hidden='true'>
-            <div className='modal-dialog modal-dialog-centered p-0 m-0 d-flex justify-content-center align-items-center'>
-                <div style={{ fontFamily: 'var(--mont)' }} className='modal-content modelProjectsContent'>
-                    <div className='modal-header'>
-                        <h2 className='p-0 m-0' style={{ fontFamily: 'var(--mont)', fontWeight: 'bold', fontSize: `${isMobile ? '4vw' : '2vw'}` }} >{title}</h2>
+        <div className="modal modalProject fade p-0 m-0" id={id} data-bs-backdrop="static" data-bs-keyboard="false" aria-labelledby="staticBackdropLabel" aria-hidden="true" >
+            <div style={{minWidth: '40vw'}} className="modal-dialog modal-dialog-centered">
+                <div style={{minWidth:'40vw'}} className="modal-content rounded-0">
+                    <div style={{padding: '1.5vw'}} className="modal-header">
+                        <h2 className='p-0 m-0' style={{ fontFamily: 'var(--mont)', fontWeight: 'bold', fontSize: `${isMobile ? '4vw' : '2.3vw'}` }} >{title}</h2>
                         <RxCross1 style={{ color: '#000', fontSize: `${isMobile ? '6.5vw' : '2vw'}` }} type="button" className="btnclose" data-bs-dismiss="modal" aria-label="Close" />
                     </div>
-                    <div className='d-flex flex-column align-items-center modal-body-projects'>
-                        <div className='d-flex justify-content-center align-items-center'>
-                            <img loading='lazy' className='imgModalProjects' src={img} alt="imgModal" />
-                        </div>
-                        <div style={{ width: `${isMobile ? '100' : '50'}` }} className='textModalContainer text-center d-flex flex-column justify-content-center align-items-center'>
-                            <div style={{ marginTop: '0vw' }} className='d-flex flex-column align-items-center textModal'>
-                                <h3 style={{ fontFamily: 'var(--mont)', fontWeight: 'bold', fontSize: `${isMobile ? '4vw' : '1.5vw'}` }} className='p-0 m-0'>Description:</h3>
-                                <p style={{ fontFamily: 'var(--mont)', fontSize: `${isMobile ? '3vw' : '1vw'}`, margin: '2vw 0 0 0 ' }} className='p-0'>{desc}</p>
-                            </div>
-                        </div>
+                    <div className="modal-body p-0">
+                        <img loading='lazy' className='imgModalProjects' src={img} alt="imgModal" />
                     </div>
-                    <div style={{ gap: '3vw', padding: `${isMobile ? '6vw' : '0vw'}` }} className=' d-flex flex-row justify-content-center align-items-center'>
-                        <h3 style={{ fontFamily: 'var(--mont)', fontWeight: 'bold', fontSize: `${isMobile ? '4vw' : '1.5vw'}` }} className='p-0 m-0 '>Tecnologies:</h3>
-                        <div style={{ height: '2VW', gap: `${isMobile ? '1vw' : '1vw'}` }} className='d-flex align-items-center justify-content-between modalFooter'>
+                    <div className="modal-footer d-flex justify-content-between">
+                        <div className='w-50 m-0'>
+                            <h3 style={{fontFamily: 'var(--mont)',fontSize:  `${isMobile ? '3.5vw' : '1.5vw'}`}} className='m-0 fw-bold text-center'>Tecnologies:</h3>
+                        </div>
+                        <div style={{padding: '.5vw', gap: '1vw'}} className='w-50 m-0 d-flex justify-content-center'>
                             <img loading='lazy' className='imgTecnoModal' src={tecno1} alt="tecno" />
                             <img loading='lazy' className='imgTecnoModal' src={tecno2} alt="tecno" />
                             <img loading='lazy' className='imgTecnoModal' src={tecno3} alt="tecno" />
@@ -82,24 +74,28 @@ const ModalProject = ({ id, title, desc, img, tecno1, tecno2, tecno3, tecno4 }) 
                 </div>
             </div>
         </div>
+
     );
 }
 
 const ProjectsWebDevelopment = () => {
     return (
         <>
-            <ProjectItem id={'multi'} img={multityr} url={Pagina_multityr} />
-            <ProjectItem id={'calcu'} img={imgCalcu} url={calcu} />
-            <ProjectItem id={'cont'} img={imgContador} url={contador} />
-            <ProjectItem id={'todo'} img={imgToDo} url={toDo} />
-            <ProjectItem id={'php'} img={imgPhp} url={phpCrud} />
+            <ProjectItem urlPage={'https://multiserviciostyr.netlify.app/'} urlGit={'https://github.com/lorenzoJimenez301/MultiserviciosTyR.git'} id={'multi'} img={multityr} />
+            <ProjectItem urlPage={'https://calculator-react-lorenzo.netlify.app'} urlGit={'https://github.com/lorenzoJimenez301/Calculadora-React.git'} id={'calcu'} img={imgCalcu} />
+            <ProjectItem urlPage={'https://contador-clicks-lorenzo.netlify.app/'} urlGit={'https://github.com/lorenzoJimenez301/Contador-Clicks.git'} id={'cont'} img={imgContador} />
+            <ProjectItem urlPage={'https://gestor-tareas-lorenzo.netlify.app/'} urlGit={'https://github.com/lorenzoJimenez301/Gestor-Tareas.git'} id={'todo'} img={imgToDo} />
+            <ProjectItem id={'php'} img={imgPhp} />
+            <ProjectItem urlPage={'https://fake-store-api-lorenzo-jimenez.netlify.app/'} urlGit={'https://github.com/lorenzoJimenez301/Fake-Store-Api.git'} id={'fakeStore'} img={fakeStore} />
         </>
     );
 }
 
 const ProjectsGrapich = () => {
     return (
-        <><h4 style={{color: 'var(--negro)'}}>Coming Soon! :3</h4></>
+        <div className='grapichContainer'>
+            <h4 style={{ color: 'var(--negro)' }}>Coming Soon! :3</h4>
+        </div>
     );
 }
 
@@ -111,12 +107,12 @@ export const Projects = () => {
     }
     const { backgroundImage } = useContext(ThemeContext);
     return (
-        <section style={{backgroundImage: `url(${backgroundImage})`}} className='projectsSection'>
+        <section style={{ backgroundImage: `url(${backgroundImage})` }} className='projectsSection'>
             <div className='projectsTitleContainer'>
                 <h2 className='projectTitle p-0 m-0'>Projects</h2>
                 <h3 className='projectSubTitle p-0 m-0'>Personal & Professional</h3>
             </div>
-            <div className='projectsContainer'>
+            <div className='projectsContainer h-100'>
                 <div className='projectsCate d-flex flex-row justify-content-start align-items-center'>
                     <button className={`btnProjects ${!isGraphicDesign ? 'selected' : ''}`} onClick={() => handleChange(false)}>Web Development</button>
                     <button className={`btnProjects ${isGraphicDesign ? 'selected' : ''}`} onClick={() => handleChange(true)}>Grapich Design</button>
@@ -128,6 +124,7 @@ export const Projects = () => {
                     <ModalProject id={'cont'} img={imgContador} desc={'A counter made with React is a UI component that uses the React JavaScript library to create a web application that displays a number and allows you to increase or decrease it with increment and decrement buttons.'} title={'React Counter'} tecno1={Html} tecno2={Css} tecno3={react} tecno4={Node} />
                     <ModalProject id={'todo'} img={imgToDo} desc={'A task manager made with React is a web application that uses the React JavaScript library to help users dynamically organize, create, delete, and edit tasks.'} title={'React "Thing to do"'} tecno1={Html} tecno2={Css} tecno3={react} tecno4={Node} />
                     <ModalProject id={'php'} img={imgPhp} desc={'A CRUD (Create, Read, Update, Delete) in PHP is a set of basic operations to manage data in a database.'} title={'PHP CRUD'} tecno1={Html} tecno2={Css} tecno3={Bootstrap} tecno4={Php} />
+                    <ModalProject id={'fakeStore'} img={fakeStore} desc={''} title={'Fake-Store-API'} tecno1={Html} tecno2={apiLogo} tecno3={Bootstrap} tecno4={react} />
                 </div>
             </div>
         </section>
